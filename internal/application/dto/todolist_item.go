f68d374d7b58a1c576d0c7715f8d6d31d5bb2826
Muhammad Ardan Hilal <muhammadardanhilal@MacBook-Air-Muhammad-2.local>
2025-12-22 13:58:50 +0700

    update project todolist items dto

--- internal/application/dto/todolist_item.go
+++ internal/application/dto/todolist_item.go
@@ -17,7 +17,7 @@ type CreateProjectTodolistItemsDto struct {
 	ProjectTodolistID string `json:"project_todolist_id" validate:"required"`
 	Name              string `json:"name" validate:"required"`
 	CategoryName      string `json:"category_name" validate:"required"`
-	IsCompleted       bool   `json:"is_completed" validate:"required"`
+	IsCompleted       *bool  `json:"is_completed" validate:"required"`
 }
 type UpdateProjectTodolistItemsDto struct {
 	ID           string `json:"id" validate:"required"`
